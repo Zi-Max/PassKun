22e76e1912861cf1fc6b9c0c0c3c1f5f04c2ed61
vagrant <vagrant@linux-vag20>
2023-10-14 11:44:37 +0000

    Lint main file

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 """TempGame main logic file"""
 ###### Python Packges ######
-import sys
 import typer
 from typing_extensions import Annotated
 from cryptography.fernet import Fernet
 
 ###### Path Append ######
-sys.path.append("../PyEngine_v2")
+# sys.path.append("../PyEngine_v2")
 
 ###### My Packges ######
-from Libs.json_handler import read_json, append_json
+# from Libs.json_handler import read_json, append_json
+from json_handler import read_json, append_json
 
 app = typer.Typer()
 
@@ -24,13 +24,15 @@ def generate_key(name: Annotated[str, typer.Argument()]):
     """
     key = Fernet.generate_key()
 
-    with open(name, "wb") as w:
-        w.write(key)
+    with open(name, "wb") as file:
+        file.write(key)
 
 
 @app.command()
-def add_record(key_path: Annotated[str, typer.Argument()], file_path: Annotated[str, typer.Argument()],
-                record_name: Annotated[str, typer.Argument()], record: Annotated[str, typer.Argument()]):
+def add_record(key_path: Annotated[str, typer.Argument()],
+                file_path: Annotated[str, typer.Argument()],
+                record_name: Annotated[str, typer.Argument()],
+                record: Annotated[str, typer.Argument()]):
     """
     Add a new record
 
@@ -39,8 +41,8 @@ def add_record(key_path: Annotated[str, typer.Argument()], file_path: Annotated[
         file_path: file path to save data into it
         data: given data to save
     """
-    with open(key_path, "rb") as r:
-        key_data = r.read()
+    with open(key_path, "rb") as file:
+        key_data = file.read()
 
     key_obj = Fernet(key_data)
     record_data = {}
@@ -56,7 +58,8 @@ def add_record(key_path: Annotated[str, typer.Argument()], file_path: Annotated[
 
 
 @app.command()
-def show_records(key_path: Annotated[str, typer.Argument()], file_path: Annotated[str, typer.Argument()]):
+def show_records(key_path: Annotated[str, typer.Argument()],
+                file_path: Annotated[str, typer.Argument()]):
     """
     Display all the given file records
 
@@ -64,8 +67,8 @@ def show_records(key_path: Annotated[str, typer.Argument()], file_path: Annotate
         key_path: key file path to decrypt data with it
         file_path: file path to display data from it
     """
-    with open(key_path, "rb") as r:
-        key_data = r.read()
+    with open(key_path, "rb") as file:
+        key_data = file.read()
 
     key_obj = Fernet(key_data)
     json_data = read_json(file_path)
